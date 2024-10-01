981391a93912117df93480ee4bdbeb19cd5e27dc
salvagrass <salvatore2.grasso@mail.polimi.it>
2024-10-01 15:28:05 +0000

    Finished second task

--- Challenge1/main.cpp
+++ Challenge1/main.cpp
@@ -1,13 +1,18 @@
 #include <Eigen/Dense>
 #include <iostream>
+#include <random>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define MARGIN 50
+
 using namespace std;
 using namespace Eigen;
+
+int saveToFile(MatrixXd imageMatrix, int width, int height, const std::string ouputFileName);
 
 int main(int argc, char** argv)
 {
@@ -23,7 +28,38 @@ int main(int argc, char** argv)
     }
 
     cout << "Matrix dimension: " << gscale.rows() << "x" << gscale.cols() << endl;
+    
+    
     //Task 2
-
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(-MARGIN,MARGIN);
+    MatrixXd noisy(height,width);
+    for(int i=0;i<height;i++){
+        for(int j=0;j<width;j++){
+            int number = distribution(generator);
+            noisy(i,j) = gscale(i,j) + distribution(generator);
+            if(noisy(i,j)<0)
+                noisy(i,j)=0;
+            else if(noisy(i,j)>255)
+                noisy(i,j)=255;
+        }
+    }
+    
+    cout << (saveToFile(noisy,width,height,"noisy.png") == 0  ? "Exported noisy image" : "Error Occured") << endl;
+    
     return 0;
+}
+
+int saveToFile(MatrixXd imageMatrix, int width, int height, const std::string ouputFileName)
+{
+  Matrix<unsigned char, Dynamic, Dynamic, RowMajor> outputImage(width, height);
+  outputImage = imageMatrix.unaryExpr([](double val) -> unsigned char
+                                      { return static_cast<unsigned char>(val); });
+
+  if (stbi_write_png(ouputFileName.c_str(), width, height, 1, outputImage.data(), width) == 0)
+  {
+    return 1;
+  }
+
+  return 0;
 }
