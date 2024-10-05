f4359c94fa9b0942aba5690d7bd5d19ed781a86a
salvagrass <salvatore2.grasso@mail.polimi.it>
2024-10-05 10:04:25 +0000

    Finished tasks and refined the code

--- Challenge1/main.cpp
+++ Challenge1/main.cpp
@@ -1,6 +1,7 @@
 #include <Eigen/Eigen>
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
+#include <unsupported/Eigen/SparseExtra>
 #include <iostream>
 #include <random>
 
@@ -18,7 +19,7 @@ int saveToFile(MatrixXd imageMatrix, int height, int width, const std::string ou
 {
     Matrix<unsigned char, Dynamic, Dynamic, RowMajor> outputImage(width, height);
     outputImage = imageMatrix.unaryExpr([](double val) -> unsigned char
-                                        { return static_cast<unsigned char>(std::max(std::min(val,255.0),0.0)); });
+                                        { return static_cast<unsigned char>(std::clamp(val,0.0,255.0));});
 
     if (stbi_write_png(ouputFileName.c_str(), width, height, 1, outputImage.data(), width) == 0)
     {
@@ -28,9 +29,9 @@ int saveToFile(MatrixXd imageMatrix, int height, int width, const std::string ou
     return 0;
 }
 
-SparseMatrix<double> convToM(Matrix3d convM, int Aheight, int Awidth)
+SparseMatrix<double,RowMajor> convToM(Matrix3d convM, int Aheight, int Awidth)
 {
-    SparseMatrix<double> m(Aheight * Awidth, Aheight * Awidth);
+    SparseMatrix<double,RowMajor> m(Aheight * Awidth, Aheight * Awidth);
     m.reserve(VectorXd::Constant(Aheight*Awidth,9));
     for (int i = 0; i < Aheight * Awidth; i++)
     {
@@ -98,17 +99,9 @@ int main(int argc, char **argv)
     uniform_int_distribution<int> distribution(-MARGIN, MARGIN);
     MatrixXd noisy(height, width);
     for (int i = 0; i < height; i++)
-    {
         for (int j = 0; j < width; j++)
-        {
-            int number = distribution(generator);
             noisy(i, j) = gscale(i, j) + distribution(generator);
-            if (noisy(i, j) < 0)
-                noisy(i, j) = 0;
-            else if (noisy(i, j) > 255)
-                noisy(i, j) = 255;
-        }
-    }
+        
 
     cout << (saveToFile(noisy, height, width, "noisy.png") == 0 ? "Exported noisy image" : "Error Occured") << endl;
     
@@ -122,7 +115,7 @@ int main(int argc, char **argv)
     //Task 4 (Hav2)
     Matrix3d Hav2 = Matrix3d::Ones();
     Hav2 /= 9.0;
-    SparseMatrix<double> A1 = convToM(Hav2, height, width);
+    SparseMatrix<double,RowMajor> A1 = convToM(Hav2, height, width);
     cout << "Non-zero entries in A1:" << A1.nonZeros() << endl;
     cout << "Is A1 Symmetric: " << A1.isApprox(A1.transpose()) << endl;
 
@@ -137,7 +130,7 @@ int main(int argc, char **argv)
     Hsh2 << 0, -3, 0,
             -1, 9,-3,
             0, -1, 0; 
-    SparseMatrix<double> A2 = convToM(Hsh2, height, width);
+    SparseMatrix<double,RowMajor> A2 = convToM(Hsh2, height, width);
     cout << "Non-zero entries in A2:" << A2.nonZeros() << endl;
     cout << "Is A2 Symmetric: " << A2.isApprox(A2.transpose()) << endl;
 
@@ -146,13 +139,24 @@ int main(int argc, char **argv)
     sharpenedv = A2 * v;
     MatrixXd sharpened = Map<MatrixXd>(sharpenedv.data(),height,width);
     cout << (saveToFile(sharpened, height, width, "sharpened.png") == 0 ? "Exported sharpened image" : "Error Occured") << endl;
+
+    //Task 8 (Exporting A2 and w)
+    saveMarket(A2,"A2.mtx");
+    saveMarketVector(w,"w.mtx");
+
+    //Task 9 (Importing x and exporting it as .png)
+    VectorXd x;
+    loadMarketVector(x,"x.mtx");
+    MatrixXd firstSysSol = Map<MatrixXd>(x.data(),height,width);
+    cout << (saveToFile(firstSysSol, height, width, "firstSysSol.png") == 0 ? "Exported first solution as an image" : "Error Occured") << endl;
+
     
     //Task 10 (Hlap)
     Matrix3d Hlap;
     Hlap << 0, -1, 0,
             -1, 4,-1,
             0, -1, 0; 
-    SparseMatrix<double> A3 = convToM(Hlap, height, width);
+    SparseMatrix<double,RowMajor> A3 = convToM(Hlap, height, width);
     cout << "Non-zero entries in A3:" << A3.nonZeros() << endl;
     cout << "Is A3 Symmetric: " << A3.isApprox(A3.transpose()) << endl;
 
@@ -161,5 +165,25 @@ int main(int argc, char **argv)
     laplacev = A3 * v;
     MatrixXd laplace = Map<MatrixXd>(laplacev.data(),height,width);
     cout << (saveToFile(laplace, height, width, "laplace.png") == 0 ? "Exported laplace image" : "Error Occured") << endl;
+
+
+    //Task 12 (Solve sparse linear system)
+    SparseMatrix<double,RowMajor> A = A3;
+    for(int i=0;i<A.rows();i++)
+        A.coeffRef(i,i) += 1;
+    BiCGSTAB<SparseMatrix<double,RowMajor>> solver;
+    solver.setTolerance(1e-10);
+    solver.compute(A);
+    VectorXd y = solver.solve(w);
+    double residual =  (w-A*y).norm();
+
+    std::cout << "Solved in " << solver.iterations() << " iterations" << std::endl;
+    std::cout << "Final residual is " << residual << std::endl;
+
+    //Task 13 (Export the solution)
+    MatrixXd secSysSol = Map<MatrixXd>(y.data(),height,width);
+    cout << (saveToFile(secSysSol, height, width, "secSysSol.png") == 0 ? "Exported second solution as an image" : "Error Occured") << endl;
+
+
     return 0;
 }
